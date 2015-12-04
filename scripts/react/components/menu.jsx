ba3d92dd6578ad7ac2a680f1c1d07edc0b1a8433
Thomas Charlat <thomas.charlat@gmail.com>
2015-12-04 17:00:55 +0100

    menu component reshaped a little bit

--- scripts/react/components/menu.jsx
+++ scripts/react/components/menu.jsx
@@ -2,15 +2,29 @@ function urlify (url) {
   return 'url(' + R(url) + ')';
 }
 
+var MenuItem = React.createClass({
+  render:function () {
+    var style = {
+      backgroundImage: urlify(this.props.data.url)
+    }
+    return <div className='menu-item' style={style}/>
+  }
+})
+
 module.exports = React.createClass({
   render: function () {
+    var icons = [
+      '/images/ic_menu_black_48dp_1x.png',
+      '/images/ic_expand_more_black_48dp_1x.png',
+      '/images/ic_expand_less_black_48dp_1x.png',
+      '/images/logoV12_center_256gold_transparent.png'
+    ]
+    var inner = icons.map(function (url) {
+      return <MenuItem data={{url:url}}/>
+    })
+
     return (
-      <div className='mobile-menu'>
-        <div className='menu-item' style={{backgroundImage: urlify('/images/ic_menu_black_48dp_1x.png')}}/>
-        <div className='menu-item' style={{backgroundImage: urlify('/images/ic_expand_more_black_48dp_1x.png')}}/>
-        <div className='menu-item' style={{backgroundImage: urlify('/images/ic_expand_less_black_48dp_1x.png')}}/>
-        <div className='menu-item' style={{backgroundImage: urlify('/images/logoV12_center_256gold_transparent.png')}}/>
-      </div>
+      <div className='mobile-menu'>{inner}</div>
     );
   }
 });
