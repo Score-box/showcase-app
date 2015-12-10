b28904a2eebeae671364f5bc8c830f0d3b134f36
LumenKairos <nathanelalouf@gmail.com>
2015-12-10 14:10:43 +0100

    Fixes indentation issues in the CTA component

--- scripts/react/components/Cta.jsx
+++ scripts/react/components/Cta.jsx
@@ -1,29 +1,29 @@
 module.exports = React.createClass({
-	render: function () {
-		var currentPage = this.props.currentPage
-	
-		var style = {}
-		
-	
-		if(currentPage === 0) {
-			style.visibility = 'visible' 
-		}else {
-			style.visibility = this.props.visibility ? 'visible' : 'hidden'
-		}
-		
-		
-		return (
-			<div className='cta-togglable' style={style}>
-				<h3>Découvrez notre beta en avant première </h3>
-				<form action="#" method="POST">
-					<fieldset>
-						<input type="email" id="email" placeholder="Email" required autofocus></input>
-					</fieldset>
-					<fieldset>
-						<input type="submit"></input>
-					</fieldset>
-				</form>
-			</div>
-			);
-	}
+  render: function () {
+    var currentPage = this.props.currentPage
+  
+    var style = {}
+    
+  
+    if(currentPage === 0) {
+      style.visibility = 'visible' 
+    }else {
+      style.visibility = this.props.visibility ? 'visible' : 'hidden'
+    }
+    
+    
+    return (
+      <div className='cta-togglable' style={style}>
+        <h3>Découvrez notre beta en avant première </h3>
+        <form action="#" method="POST">
+          <fieldset>
+            <input type="email" id="email" placeholder="Email" required autofocus></input>
+          </fieldset>
+          <fieldset>
+            <input type="submit"></input>
+          </fieldset>
+        </form>
+      </div>
+      );
+  }
 })
