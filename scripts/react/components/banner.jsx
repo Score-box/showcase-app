9f0f8c5aa86d8aa7091d1ea6021c64de52109e04
Thomas Charlat <thomas.charlat@gmail.com>
2015-12-11 11:24:16 +0100

    start to define banner style

--- scripts/react/components/banner.jsx
+++ scripts/react/components/banner.jsx
@@ -3,19 +3,21 @@ function urlify (url) {
 }
 
 var BannerClass = [
+  'm-banner',
   'm-page',
-  'm-flex-box-orient'
+  'm-flex-box-orient',
+  'm-flex-container-orient'
 ].join(' ')
 
 module.exports = React.createClass({render: function () {
 	return (
-    <div id='banner'
+    <div id='m-banner'
       className={BannerClass}
       style={{
         backgroundImage: urlify('/images/sky.jpeg'),
         backgroundSize: 'cover'
       }}
-    ><div className='title'>
+    ><div className='m-banner-title'>
         SCORE BOX
       </div>
     </div>
